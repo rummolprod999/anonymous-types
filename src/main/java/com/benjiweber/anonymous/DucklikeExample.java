f0c7317c3a8b0a4a8c9c92a9834114482dda3454
rummolprod999 <rummolprod999@gmail.com>
2020-10-06 19:41:52 +0300

    update

--- src/main/java/com/benjiweber/anonymous/DucklikeExample.java
+++ src/main/java/com/benjiweber/anonymous/DucklikeExample.java
@@ -3,11 +3,13 @@ package com.benjiweber.anonymous;
 import com.benjiweber.anonymous.AnonymousTypes.Anon;
 
 import static com.benjiweber.anonymous.AnonymousTypes.with;
+import static java.lang.System.out;
 
 public class DucklikeExample {
 
     public static void main(String... args) {
         with((Anon & Quacks & Waddles) i -> i, ducklike -> {
+            out.println(ducklike.getClass());
             ducklike.quack();
             ducklike.waddle();
         });
@@ -16,19 +18,20 @@ public class DucklikeExample {
     }
 
     public static <Ducklike extends Quacks & Waddles> void doDucklikeThings(Ducklike ducklike) {
+        out.println(ducklike.getClass());
         ducklike.quack();
         ducklike.waddle();
     }
 
     interface Quacks {
         default void quack() {
-            System.out.println("Quack");
+            out.println("Quack");
         }
     }
 
     interface Waddles {
         default void waddle() {
-            System.out.println("Waddle");
+            out.println("Waddle");
         }
     }
 }
